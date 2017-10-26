b87c78aa24c2d03a761489c4e0ebb45d45194499
Sherwino <sherwination@gmail.com>
2017-10-26 00:46:32 -0400

    added video list to the DOM

--- src/index.js
+++ src/index.js
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import API_KEY from '../keys';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-// const fs = require('fs');
-// require('dotenv').config();
+import VideoList from './components/video_list';
 
-// Be sure to upload this as an environemental variable.
-const API_KEY =  process.env.REACT_APP_ENV;
+// this youtube key is coming from keys.js
+const YT_KEY =  `${API_KEY}`;
 
 // Create a new comoponent
 // This comoponent should make some HTML
 // This is a functional based component
 // Functional components can have class based components inside
 // Constructor Constructor what is your function?
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+    // immediately search this on app startup
+    YTSearch({ key: YT_KEY, term: 'surfboards'}, (videos) => {
+      //when key and value is the same like { videos: videos } you could write it as { videos }
+      this.setState({ videos });
+    });
+
+  }
+
+// you will need to pass data from the parent App to the child component... like VideoList
+// in react that is called passing props
+  render () {
+
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={ this.state.videos } />
+      </div>
   );
+}
 }
 
 
